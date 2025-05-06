eb929a845d112d087fcdbe92c52596ff5d9e3661
Veeruby18 <odueloghen@gmail.com>
2025-05-06 18:35:36 +0100

    initail commit

---
+++ LEANING_JAVASCRIPT/index.js
@@ -0,0 +1,158 @@
+// //function reg() {
+//     //alert("you have successfully registered");
+//  // }
+
+// // alert('Welcome to JavaScript')
+// // ptompt('Enter your score')
+// // confirm('Do you wish to continue?')
+
+// // let x = 30
+// // let y = 5
+// // let ans = x*=5
+// // console.log(ans)
+
+// //Data type
+// //Primitive data types
+// //Number data type
+// // let num = 50
+// // console.log (num);
+
+// //String DT
+// let surname = 'Odu'
+// // console.log (surname);
+
+// //Boolean DT
+// let isMarried = true
+// console.log (isMarried);
+
+// //Null DT
+// let religion = null
+// console.log(religion);
+
+// //undefined DT
+// let birthDate;
+// console.log(birthDate);
+
+// //Non-primitive DT
+// //Date DT
+// let time = new Date().getHours
+// console.log(time);
+
+
+// //Function DT
+// function addNum (a, b){
+// console. log (a - b);
+// }
+// addNum(4, 3)
+// addNum(12, 5)
+// addNum(8, 10)
+
+// //Arrays DT     
+// //lenght           1        2       3       4             
+// let colours = ['black', 'white', 'blue', 'gray']
+// //index            0        1       2       3 
+// console.log(colours)
+// //array methods
+// colours.push('violet')
+// //colours.pop()
+// //colours.shift()
+// colours.unshift('green')
+// console.log(colours)
+// colours.slice(2,3) //slice remove an index of your choice//
+// console.log(colours)
+// console.log(colours.length)
+// console.log(colours [0])
+
+// //Object DT 
+// // const person ={
+// //     name: 'Vantia Odu',
+// //     Age: 30,
+// //     religion: 'christian',
+// //     children: null,
+// //     graduate: true
+// // }
+
+// //console.log(person['name'], person.graduate);
+
+// //Nested arrays
+// const properties =[
+//     'clothes', 'shoes',
+//     cars = ['Porshe', 'Toyota', 'Audi', benz =['GKL', 'AMG', 'CL'] ],
+//     companies = ['Tech-hub', 'Cafe', resturants =['sweet sensation', 'Bukka Hurt',
+//         'Dominoes']],
+//         houses=['Bungalow', 'duplex', condos=['1-storey', '2-storey'], 'penthouse']
+
+// ]
+
+// console.log(properties[4][2][1]);
+// console.log(condos[1]);
+
+
+
+// //Nested objects
+// const person ={
+//     name: 'Vantia Odu',
+//     Age: 30,
+//     religion: 'christian',
+//     children: null,
+//     graduate: true,
+//     job: {
+//         title: 'software Engineer',
+//         company:'Google',
+//         level: 'Tier 1',
+//         certificate: 'ISBQT',
+//         details: {
+//             startDate: '20/May/2018',
+//             endDate: 'Currently works here',
+//             team: 'Alpha One'
+//         }
+//    }
+// }
+
+// console.log(person.job.certificate);
+// console.log(person.job.details.team);
+
+// //Arithmetic Operators
+// console.log(7 + 5);
+// console.log(7 - 5);
+// console.log(7 / 5);
+// console.log(7 * 5);
+// console.log(7 ** 5); //exponential operator
+// console.log(7 % 5); //modulus operator (it target the remainder of a division)
+
+//Shorthand operators
+// let price = 30
+// let quantity = 2
+// // price *= quantity
+// let discount = price * 0.25
+// price -= discount
+// // price += quantity
+// // price -= quantity
+// console.log(price);
+
+// //Comparion Operators
+// console.log(5 == '5');
+// console.log(5 != '5');
+// console.log(5 === '5');
+// console.log(5 !== '5');
+// console.log(5 > 4);
+// console.log(5 < 6);
+// console.log(5 >= 8);
+// console.log(5 <= 9);
+
+//Logical operators 
+//And operator &&
+let email = 'vantia@email.com'
+let password = 12345
+console.log(email === 'Vantia@email.com' && password === 12345);
+
+//Or operator ||
+console.log(email === 'Vantia@email.com' || password === 12345);
+
+
+
+
+
+
+
+
